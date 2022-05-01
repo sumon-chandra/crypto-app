6f898efaf14ac7a8a8cab991b1f3592f11964d3e
Sumon Chandra Shil <topu199041@gmail.com>
2022-05-01 15:14:07 +0600

    Added Chart for crypto details

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,27 +1,85 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
 import { Col, Row, Typography } from "antd";
-const { Title } = Typography;
 
-function LineChart({ coinHistory, currentPrice, coinName }) {
-  console.log(coinHistory);
+const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+  const coinPrice = [];
+  const coinTimestamp = [];
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinPrice.push(coinHistory?.data?.history[i].price);
+  }
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+    coinTimestamp.push(
+      new Date(
+        coinHistory?.data?.history[i].timestamp * 1000
+      ).toLocaleDateString()
+    );
+  }
+
+  const data = {
+    labels: coinTimestamp.reverse(),
+    datasets: [
+      {
+        label: "Price In USD",
+        data: coinPrice.reverse(),
+        fill: false,
+        backgroundColor: "#0071bd",
+        borderColor: "#0071bd",
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
   return (
-    <div>
+    <>
       <Row className="chart-header">
-        <Title level={2} className="chart-title">
-          {coinName} Price Chart
-        </Title>
+        <Typography.Title level={2} className="chart-title">
+          {coinName} Price Chart{" "}
+        </Typography.Title>
         <Col className="price-container">
-          <Title level={5} className="price-change">
-            {coinHistory?.data?.change}
-          </Title>
-          <Title level={5} className="current-price">
-            Current {coinName} price: $ {currentPrice}
-          </Title>
+          <Typography.Title level={5} className="price-change">
+            Change: {coinHistory?.data?.change}%
+          </Typography.Title>
+
+          <Typography.Title level={5} className="current-price">
+            Current {coinName} Price: $ {currentPrice}
+          </Typography.Title>
         </Col>
       </Row>
-    </div>
+      <Line data={data} options={options} />
+    </>
   );
-}
+};
 
 export default LineChart;
